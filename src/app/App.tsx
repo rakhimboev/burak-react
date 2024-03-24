c9e691cc210923f28cf3d813bb063160d2fc8678
rakhimboev <dilshodbekrakhimbaev@gmail.com>
2024-03-24 15:57:05 -0400

    feat: MUI standard integration

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import "../css/app.css";
+import { Box, Button, Container, Stack, Typography } from "@mui/material";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <Container maxWidth="sm">
+      <Stack flexDirection={"column"}>
+        <Box sx={{ my: 4 }}>
+          <Typography variant="h4" component={"h4"}>
+            Hello World
+          </Typography>
+        </Box>
+        <Button variant="contained">submit</Button>
+      </Stack>
+    </Container>
+  );
 }
 
 export default App;
