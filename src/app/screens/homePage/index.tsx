696b634c8f773e17f835d3c01d18dd8f3a95fd78
rakhimboev <dilshodbekrakhimbaev@gmail.com>
2024-04-21 19:48:14 -0400

    feat: create HomaPage Reduc configuration

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -5,8 +5,14 @@ import Advertisement from "./Advertisement";
 import ActiveUsers from "./ActiveUsers";
 import Events from "./Events";
 import "../../../css/home.css";
+import { useEffect } from "react";
 
 export const HomePage = () => {
+  //Selector: Store => Data
+  useEffect(() => {
+    // backend server data request => Data
+    // Slice: data => Store
+  }, []);
   return (
     <div className="homepage">
       <Statistics />
