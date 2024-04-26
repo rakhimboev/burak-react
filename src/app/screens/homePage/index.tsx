426efe65e39e5df6ac73bdffcdd9f5b726f4b84a
rakhimboev <dilshodbekrakhimbaev@gmail.com>
2024-04-26 06:12:18 -0400

    feat: develop product service and retrieve data from back end

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect } from "react";
 import Statistics from "./Statistics";
 import PopularDishes from "./PopularDishes";
 import NewDishes from "./NewDishes";
@@ -5,36 +6,39 @@ import Advertisement from "./Advertisement";
 import ActiveUsers from "./ActiveUsers";
 import Events from "./Events";
 import "../../../css/home.css";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Dispatch } from "@reduxjs/toolkit";
-import { createSelector } from "reselect";
-import { setPopularDishes } from "./slice";
-import { retrievePopularDishes } from "./selector";
-import { Product } from "../../../lib/types/product";
 
+import { useDispatch } from "react-redux";
+import { Dispatch } from "@reduxjs/toolkit";
+import { setPopularDishes } from "./slice";
+import { Product } from "../../../lib/types/product";
+import ProductService from "../../services/ProductService";
+import { ProductCollection } from "../../../lib/enums/product.enum";
+
+/** REDUX SLICE & SELECTOR */
 const actionDispatch = (dispatch: Dispatch) => ({
   setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)),
 });
 
-const popularDishesRetriever = createSelector(
-  retrievePopularDishes,
-  (popularDishes) => ({ popularDishes })
-);
+export default function HomePage() {
+  const { setPopularDishes } = actionDispatch(useDispatch());
 
-export const HomePage = () => {
-  // const { setPopularDishes } = actionDispatch(useDispatch());
-  // const { popularDishes } = useSelector(popularDishesRetriever);
-
-  //Selector: Store => Data
   useEffect(() => {
-    // backend server data request => Data
-    // Slice: data => Store
-    // @ts-ignore
+    const product = new ProductService();
+    product
+      .getProducts({
+        page: 1,
+        limit: 4,
+        order: "productViews",
+        productCollection: ProductCollection.DISH,
+      })
+      .then((data) => {
+        setPopularDishes(data);
+      })
+      .catch((err) => console.log(err));
   }, []);
 
   return (
-    <div className="homepage">
+    <div className={"homepage"}>
       <Statistics />
       <PopularDishes />
       <NewDishes />
@@ -43,4 +47,4 @@ export const HomePage = () => {
       <Events />
     </div>
   );
-};
+}
