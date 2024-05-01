23cb4ab1b62405916843ee5bae3e1bb4c7d2fc7c
rakhimboev <dilshodbekrakhimbaev@gmail.com>
2024-04-30 23:16:56 -0400

    fix:modify basket business logic via customized hooks

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,51 +1,42 @@
 import React, { useState } from "react";
-import { Route, Switch, useLocation } from "react-router-dom";
-import ProductsPage from "./screens/productsPage";
-import UserPage from "./screens/userPage";
+import { Box, Button, Container, Stack, Typography } from "@mui/material";
+import { Link, Route, Switch, useLocation } from "react-router-dom";
 import HomePage from "./screens/homePage";
-import HelpPage from "./screens/helpPage";
-import Footer from "./components/footer";
-import "../css/navbar.css";
-import "../css/app.css";
-import "../css/footer.css";
+import ProductsPage from "./screens/productsPage";
+import OrdersPage from "./screens/ordersPage";
+import UserPage from "./screens/userPage";
 import HomeNavbar from "./components/headers/HomeNavbar";
 import OtherNavbar from "./components/headers/OtherNavbar";
-import OrdersPage from "./screens/ordersPage";
-import { CartItem } from "../lib/types/search";
+import Footer from "./components/footer";
+import HelpPage from "./screens/helpPage";
+import "../css/app.css";
+import "../css/navbar.css";
+import "../css/footer.css";
+import Test from "./screens/Test";
+import useBasket from "./hooks/useBasket";
 
 function App() {
-  const location = useLocation(),
-    cartJson: string | null = localStorage.getItem("cartData"),
-    currentCart = cartJson ? JSON.parse(cartJson) : [],
-    [cartItems, setCartItems] = useState<CartItem[]>(currentCart);
+  const location = useLocation();
+  const { cartItems, onAdd, onRemove, onDelete, onDeleteAll } = useBasket();
 
-  /*  handlers */
-
-  const onAdd = (input: CartItem) => {
-    const exist: any = cartItems.find(
-      (item: CartItem) => item._id === input._id
-    );
-
-    if (exist) {
-      const cartUpdate = cartItems.map((item: CartItem) =>
-        item._id === input._id
-          ? { ...exist, quantity: item.quantity + 1 }
-          : item
-      );
-      setCartItems(cartUpdate);
-      localStorage.setItem("cardData", JSON.stringify(cartUpdate));
-    } else {
-      const cartUpdate = [...cartItems, { ...input }];
-      setCartItems(cartUpdate);
-      localStorage.setItem("cardData", JSON.stringify(cartUpdate));
-    }
-  };
   return (
     <>
-       {location.pathname === "/" ? (
-        <HomeNavbar cartItems={cartItems} />
+      {location.pathname === "/" ? (
+        <HomeNavbar
+          cartItems={cartItems}
+          onAdd={onAdd}
+          onRemove={onRemove}
+          onDelete={onDelete}
+          onDeleteAll={onDeleteAll}
+        />
       ) : (
-        <OtherNavbar cartItems={cartItems} />
+        <OtherNavbar
+          cartItems={cartItems}
+          onAdd={onAdd}
+          onRemove={onRemove}
+          onDelete={onDelete}
+          onDeleteAll={onDeleteAll}
+        />
       )}
       <Switch>
         <Route path="/products">
